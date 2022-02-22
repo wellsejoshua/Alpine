7f0fb55c221ae439e9f26da1d73e408b1df86174
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-02-22 02:53:56 -0600

    Finished alpine and updated social links

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-//Call hello world
-function HelloWorld(){
-    alert("Hello World");
+//get values/string from the page
+//controller function
+function getValues(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString =  document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+
+//reverse string
+//logic function
+function reverseString(userString){
+    let revString = [];
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+}
+//display the reversed string to the user
+//view function
+function displayString(revString){
+    //write to page
+    document.getElementById("msg").innerHTML = `Your String reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
